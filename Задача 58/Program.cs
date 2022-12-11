b647a777ed1b848783bf52722f0cfff9a29b2a25
«Vadim <vvaganoff@mail.ru>
2022-12-11 17:48:51 +0400

    Добавлено решение задачи 58

---
+++ Задача 58/Program.cs
@@ -0,0 +1,111 @@
+﻿//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+
+Console.Write("Введите размер первого двухмерного массива: ");
+string strArraySize = Console.ReadLine(); //Пользователь вводит два размера: количество строк и столбцов
+char[] charSeparators = new char[] { ' ', 'x', '+', '*', 'X', '-', ',', ';' };//Определяем, чем пользователь может отделить размеры
+string[] strArrayOfArraySize = strArraySize.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+if (strArrayOfArraySize.Length != 2) //Если пользователь ввёл не два числа, то выдаём ошибку и выходим
+{
+    Console.WriteLine("У двухмерного массива два размера: количество строк и количество толбцов");
+    return;
+}
+int[] intArraySize = new int[strArrayOfArraySize.Length];             //Преобразуем строковый массив в массив целых
+for (int i = 0; i < strArrayOfArraySize.Length; i++)                  //
+{
+    intArraySize[i] = Convert.ToInt32(strArrayOfArraySize[i]);        //
+}
+
+int[,] intArrayA = new int[intArraySize[0], intArraySize[1]];
+intArrayA = FillArray(intArraySize);
+PrintArray(intArrayA);
+
+Console.Write("Введите размер второго двухмерного массива: ");
+strArraySize = Console.ReadLine(); //Пользователь вводит два размера: количество строк и столбцов
+strArrayOfArraySize = strArraySize.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+if (strArrayOfArraySize.Length != 2) //Если пользователь ввёл не два числа, то выдаём ошибку и выходим
+{
+    Console.WriteLine("У двухмерного массива два размера: количество строк и количество толбцов");
+    return;
+}
+intArraySize = new int[strArrayOfArraySize.Length];             //Преобразуем строковый массив в массив целых
+for (int i = 0; i < strArrayOfArraySize.Length; i++)                  //
+{
+    intArraySize[i] = Convert.ToInt32(strArrayOfArraySize[i]);        //
+}
+
+int[,] intArrayB = new int[intArraySize[0], intArraySize[1]];
+intArrayB = FillArray(intArraySize);
+PrintArray(intArrayB);
+
+Console.WriteLine("Умножение матриц:");
+
+PrintArray(MatrixMultiplication(intArrayA, intArrayB));
+
+int[,] FillArray(int[] intArraySize) //Функция заполняет массив случайными числами. На вход получает массив размеров, возвращает двухмерный массив
+{
+    var random = new Random();
+    var randomInt = new Random();
+    int[,] resultArray = new int[intArraySize[0], intArraySize[1]];
+    for (int i = 0; i < intArraySize[0]; i++)
+    {
+        for (int j = 0; j < intArraySize[1]; j++)
+        {
+            resultArray[i, j] = random.Next(-10, 10);
+        }
+    }
+    return resultArray;
+}
+
+void PrintArray(int[,] arr)//Метод выводит массив на экран в формате [.., .., .., ..]
+{
+
+    for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+    {
+        for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+        {
+            if (i == arr.GetUpperBound(0) && j == arr.GetUpperBound(1))
+            {
+                Console.Write($"{arr[i, j]}");
+            }
+            else
+            {
+                Console.Write($"{arr[i, j]}, ");
+            }
+
+        }
+        Console.WriteLine("");
+    }
+}
+
+int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+{
+    if (matrixA.GetLength(1) != matrixB.GetLength(0))
+    {
+        throw new Exception("Умножение не возможно! Количество столбцов первой матрицы не равно количеству строк второй матрицы.");
+    }
+
+    var matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+    for (var i = 0; i < matrixA.GetLength(0); i++)
+    {
+        for (var j = 0; j < matrixB.GetLength(1); j++)
+        {
+            matrixC[i, j] = 0;
+
+            for (var k = 0; k < matrixA.GetLength(1); k++)
+            {
+                matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+
+    return matrixC;
+}
